04e7eea929e30a6acc689931b2af7ce58a8dbe2c
Tina Egolf <tinaegolf@gmail.com>
2015-07-29 19:17:00 +0200

    First commit... Out in the open

---
+++ js/custom.js
@@ -0,0 +1,43 @@
+/* Custom JS for Code Gym 
+–––––––––––––––––––––––––––––––––––––––––––––––––– 
+[Table Of Contents]
+
+	01- HOW IT WORKS
+*/
+
+
+$(document).ready(function() {
+
+/* –––––––––––––––––––––––––––––––––––––––––––––––
+01 - HOW IT WORKS
+–––––––––––––––––––––––––––––––––––––––––––––––––– */	
+
+	/*Box 1 - Register */
+
+	// var worksRegisterTitle = $("#how-works #titleRegister");
+	// var worksRegisterText = $("#how-works #textRegister");
+	// var parentRegisterBox = worksRegisterTitle.parent('div');
+
+	// parentRegisterBox.hover(
+	// 	function(){
+	// 	worksRegisterText.toggle();
+	// 	worksRegisterTitle.toggle();
+	// 	parentRegisterBox.toggleClass('box1-active, box1');
+	// });
+
+	/*Box 2 - Plan */
+	
+	// var worksPlanTitle = $("#how-works #titlePlan");
+	// var worksPlanText = $("#how-works #textPlan");
+	// var parentBoxPlan = worksPlanTitle.parent('div');
+
+	// parentBoxPlan.click(function(){
+	// 	worksPlanText.toggle();
+	// 	worksPlanTitle.toggle();
+	// 	parentBoxPlan.toggleClass('box1-active, box2');
+	// });
+
+
+
+
+});
