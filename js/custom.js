db29f1b5caaf826fa43f78c946c7a7d10df3d90e
Tina Egolf <tinaegolf@gmail.com>
2015-07-30 23:26:17 +0200

    Push Register Hamburg input to Google Form

--- js/custom.js
+++ js/custom.js
@@ -63,9 +63,43 @@ function postContactToGoogle () {
 	});
 }
 
+function postRegisterHamburgToGoogle () {
+	var nameRegister = $('#registerHamburg #mainRegisterName').val();
+	var emailRegister = $('#registerHamburg #mainRegisterEmail').val();
+	var startDates = $('#datesList').val();
+	var messageRegister = $('#registerHamburg #mainRegisterComment').val();
+
+
+	$.ajax ({
+		url: "https://docs.google.com/forms/d/1UdNjaeLUutI1RCaDN9G-MULMVYd1zMHXOdkMq7vfEWs/formResponse", 
+		data: {'entry.2100843075': nameRegister, 'entry.969521207': emailRegister, 'entry.1188960316':startDates, 'entry.936694200': messageRegister},
+		type: "POST",
+		dataType: 'xml',
+		statusCode: {
+			0: function() {
+				$('#registerHamburg .modal-header').toggle();
+				$('#registerHamburg .modal-body').toggle();
+				$('#registerHamburg .modal-footer #buttonSubmit').attr("disabled", "disabled");
+				$('#registerHamburgSuccess').toggle();
+			},
+			200: function() {
+				$('#registerHamburg').toggle();
+			}
+		}
+	});
+}	
+
 $(document).ready(function() {
+	$('#registerHamburg').submit(function() {
+		postRegisterHamburgToGoogle();
+		return false;
+	})
+
 	$('#formContact').submit(function() {
 		postContactToGoogle();
 		return false;
 	})	
 });
+
+
+// TODO: 1.) 200 function for HH Register Modal, 2.) My City Register Modal
